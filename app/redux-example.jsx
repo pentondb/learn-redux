625af5bc071fe1c53c82638afb8af24ab29935b2
pentondb <derek.penton@gmail.com>
2018-12-16 20:01:42 -0600

    Convert actions to action generators

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,9 +2,9 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var nextHobbyId = 1;
-var nextMovieId = 1;
 
+// Name reducer and action generators
+// ----------------------------------------
 var nameReducer = (state = 'Anonymous', action) => {
     switch (action.type) {
         case 'CHANGE_NAME':
@@ -14,6 +14,16 @@ var nameReducer = (state = 'Anonymous', action) => {
     };
 };
 
+var changeName = (name) => {
+    return {
+        type: 'CHANGE_NAME',
+        name
+    }
+};
+
+// Hobby reducer and action generators
+// ----------------------------------------
+var nextHobbyId = 1;
 var hobbiesReducer = (state = [], action) => {
     switch (action.type) {
         case 'ADD_HOBBY':
@@ -31,6 +41,21 @@ var hobbiesReducer = (state = [], action) => {
     };
 };
 
+var addHobby = (hobby) => {
+    return {
+        type: 'ADD_HOBBY',
+        hobby
+    }
+};
+
+var removeHobby = (id) => {
+    type: 'REMOVE_HOBBY',
+        id
+};
+
+// Movie reducer and action generators
+// ----------------------------------------
+var nextMovieId = 1;
 var moviesReducer = (state = [], action) => {
     switch (action.type) {
         case 'ADD_MOVIE':
@@ -47,6 +72,21 @@ var moviesReducer = (state = [], action) => {
         default:
             return state;
     }
+};
+
+var addMovie = (title, genre) => {
+    return {
+        type: 'ADD_MOVIE',
+        title,
+        genre
+    }
+};
+
+var removeMovie = (id) => {
+    return {
+        type: 'REMOVE_MOVIE',
+        id
+    };
 };
 
 var reducer = redux.combineReducers({
@@ -73,50 +113,16 @@ var unsubscribe = store.subscribe(() => {
 var currentState = store.getState();
 console.log('currentState', currentState);
 
-store.dispatch({
-    type: 'CHANGE_NAME',
-    name: 'Herman',
-});
+store.dispatch(changeName('Herman'));
 
-store.dispatch({
-    type: 'ADD_HOBBY',
-    hobby: 'Running'
-})
+store.dispatch(addHobby('Running'));
+store.dispatch(addHobby('Swimming'));
+store.dispatch(removeHobby(2));
 
-store.dispatch({
-    type: 'ADD_HOBBY',
-    hobby: 'Swimming'
-})
+store.dispatch(addMovie('Gone with the Wind', 'Drama'));
+store.dispatch(addMovie('When Harry Met Sally', 'Romantic Comedy'));
+store.dispatch(addMovie('2001: A Space Odyssey', 'Science Fiction'));
 
-store.dispatch({
-    type: 'REMOVE_HOBBY',
-    id: 2
-});
+store.dispatch(changeName('Emily'));
 
-store.dispatch({
-    type: 'ADD_MOVIE',
-    title: 'Gone with the Wind',
-    genre: 'Drama'
-});
-
-store.dispatch({
-    type: 'ADD_MOVIE',
-    title: 'When Harry Met Sally',
-    genre: 'Romantic Comedy'
-});
-
-store.dispatch({
-    type: 'ADD_MOVIE',
-    title: '2001: A Space Odyssey',
-    genre: 'Science Fiction'
-});
-
-store.dispatch({
-    type: 'CHANGE_NAME',
-    name: 'Emily',
-});
-
-store.dispatch({
-    type: 'REMOVE_MOVIE',
-    id: 2
-});
+store.dispatch(removeMovie(2));
